7586cd4d3fb070bdfc2cb2a16756a33c0f95ec16
Sourav-Dn-p <sdsouravdebnath26@gmail.com>
2025-02-26 21:04:53 +0600

    trying for render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://task-manager-13e7e.web.app"],
+    origin: ["http://localhost:5173", "https://task-manager-13e7e.web.app"],
     credentials: true,
   },
 });
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://task-manager-13e7e.web.app"],
+    origin: ["http://localhost:5173", "https://task-manager-13e7e.web.app"],
     credentials: true,
   })
 );
